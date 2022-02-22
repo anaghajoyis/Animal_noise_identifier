4e7f463f66280449f9daf2566be6ee8326fcc83a
Anagha <82244272+anaghajoyis@users.noreply.github.com>
2022-02-22 19:20:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,5 +6,28 @@ function modelReady(){
     classifier.classify(gotResults);
 }
 function gotResults(error,results){
-
+    if (error){
+        console.error(error);
+    }else{
+    console.log(results);
+    var r = Math.floor((Math.random()*255)+1);
+    var g = Math.floor((Math.random()*255)+1);
+    var b = Math.floor((Math.random()*255)+1);
+    document.getElementById("rec_sound").innerHTML=results[0].label;
+    document.getElementById("rec_accuracy").innerHTML=((results[0].confidence*100).toFixed(2))+"%";
+    document.getElementById("rec_sound").style.color="rgb("+r+","+g+","+b+")";
+    document.getElementById("rec_accuracy").style.color="rgb("+r+","+g+","+b+")";
+    var img=document.getElementById("ear1");
+    if (results[0].label=="Mewing"){
+        img.src="IMG-20220207-WA0004.jpg";
+    } else if(results[0].label=="Barking"){
+        img.src="https://wallpaperaccess.com/full/1101027.jpg";
+    }else if(results[0].label=="Roaring"){
+        img.src="https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg";
+    }else if(results[0].label=="Mooing"){
+        img.src="https://img-9gag-fun.9cache.com/photo/a07g14L_460s.jpg";
+    }else{
+        img.src="2022_02_22_0w4_Kleki.png";
+    }
+    }
 }
